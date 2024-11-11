0a7f15bfc0e71f18977ccdb1d80f75dd94a895bc
nb150301 <anhnguyen150301@gmail.com>
2024-11-11 23:04:58 +0700

    chat service

--- service/service.go
+++ service/service.go
@@ -19,6 +19,7 @@ type ServiceContainer struct {
 	VehicleService      IVehicleService
 	RideService         IRideService
 	NotificationService INotificationService
+	ChatService         IChatService
 }
 
 type ServiceFactory struct {
@@ -60,6 +61,7 @@ func (f *ServiceFactory) CreateServices() *ServiceContainer {
 		VehicleService:      f.createVehicleService(),
 		RideService:         f.createRideService(),
 		NotificationService: f.createNotificationService(),
+		ChatService:         f.createChatService(),
 	}
 }
 
@@ -85,4 +87,9 @@ func (f *ServiceFactory) createRideService() IRideService {
 
 func (f *ServiceFactory) createNotificationService() INotificationService {
 	return NewNotificationService(f.repos.NotificationRepository, f.cfg)
+}
+
+func (f *ServiceFactory) createChatService() IChatService {
+	manager := NewManager()
+	return NewChatService(f.repos.ChatRepository, f.cfg, manager)
 }
